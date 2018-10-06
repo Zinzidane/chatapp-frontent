db234774c0e1941c370a198f66e321cb9f725d4c
Zinzidane <ya-gans@mail.ru>
2018-10-07 00:11:09 +0300

    Add post service

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post-form',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  postForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private postService: PostService) { }
 
   ngOnInit() {
+    this.init();
   }
 
+  init() {
+    this.postForm = this.fb.group({
+      post: ['', Validators.required]
+    });
+  }
+
+  submitPost() {
+    this.postService.addPost(this.postForm.value).subscribe(data => {
+
+    });
+  }
 }
