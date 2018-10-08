af49f39ed8e25a7014480e206f81522ae42c8109
Zinzidane <ya-gans@mail.ru>
2018-10-08 19:35:08 +0300

    Add like functionality

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service';
 import * as moment from 'moment';
 import io from 'socket.io-client';
+import _ from 'lodash';
+import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-posts',
@@ -11,12 +13,15 @@ import io from 'socket.io-client';
 export class PostsComponent implements OnInit {
   socket: any;
   posts = [];
+  user: any;
 
-  constructor(private postService: PostService) {
+  constructor(private postService: PostService, private tokenService: TokenService) {
     this.socket = io('http://localhost:3000');
   }
 
   ngOnInit() {
+    // Get user for later usage in CheckInLikesArray
+    this.user = this.tokenService.GetPayload();
     this.AllPosts();
 
     this.socket.on('refreshPage', (data) => {
@@ -34,7 +39,13 @@ export class PostsComponent implements OnInit {
     return moment(time).fromNow();
   }
 
-  LikePost(post) {
+  CheckInLikesArray(arr, username) {
+    return _.some(arr, {username: username});
+  }
 
+  LikePost(post) {
+    this.postService.addLike(post).subscribe(data => {
+      this.socket.emit('refresh', {});
+    }, err => console.log(err));
   }
 }
