8a57851b35eeffc3ce6e2e2083248d3ca0767889
Zinzidane <ya-gans@mail.ru>
2018-10-08 19:56:01 +0300

    Add layout and styles to comments component

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent implements OnInit {
+export class CommentsComponent implements OnInit, AfterViewInit {
+  toolbarElement: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.toolbarElement = document.querySelector('.nav-content');
+  }
+
+  ngAfterViewInit() {
+    this.toolbarElement.style.display = 'none';
   }
 
 }
