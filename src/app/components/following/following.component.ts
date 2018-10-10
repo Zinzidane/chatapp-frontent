9188f7ee24991eedd7a9fc7b6e817cd6cf1e99df
Zinzidane <ya-gans@mail.ru>
2018-10-10 14:00:19 +0300

    Display user followed

--- src/app/components/following/following.component.ts
+++ src/app/components/following/following.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-following',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./following.component.css']
 })
 export class FollowingComponent implements OnInit {
+  following = [];
+  user: any;
 
-  constructor() { }
+  constructor(private tokenService: TokenService, private usersService: UsersService) { }
 
   ngOnInit() {
+    this.user = this.tokenService.GetPayload();
+    this.GetUser();
   }
 
+  GetUser() {
+    this.usersService.GetUserById(this.user._id).subscribe(data => {
+      this.following = data.result.following;
+    }, err => {
+      console.log(err);
+    });
+  }
 }
