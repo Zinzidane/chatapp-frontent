4eece250bc2687a816699a7c1e8e5b1bc67a6d55
Zinzidane <ya-gans@mail.ru>
2018-10-11 21:46:55 +0300

    Add send message method to message component

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TokenService } from 'src/app/services/token.service';
+import { MessageService } from 'src/app/services/message.service';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  messageForm: FormGroup;
+  receiver: any;
+  user: any;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private tokenService: TokenService,
+    private usersService: UsersService,
+    private messageService: MessageService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.GetUserByUsername(params.name);
+    });
+    this.user = this.tokenService.GetPayload();
+    this.init();
   }
 
+  init() {
+    this.messageForm = this.fb.group({
+      message: ['', Validators.required]
+    });
+  }
+
+  GetUserByUsername(username) {
+    this.usersService.GetUserByName(username).subscribe(data => {
+      this.receiver = data.result;
+    });
+  }
+
+  SendMessage() {
+    this.messageService.SendMessage(this.user._id, this.receiver._id, this.receiver.username, this.messageForm.value.message).subscribe(data => {
+
+    });
+  }
 }
