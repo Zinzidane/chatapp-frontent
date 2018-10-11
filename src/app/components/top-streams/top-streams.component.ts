0ef7efb7ea109e11fb2f90a096c13808f42664ea
Zinzidane <ya-gans@mail.ru>
2018-10-11 14:48:32 +0300

    Add layout and styles to top streams component

--- src/app/components/top-streams/top-streams.component.ts
+++ src/app/components/top-streams/top-streams.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+import { TokenService } from 'src/app/services/token.service';
+import { Router } from '@angular/router';
+import * as moment from 'moment';
+import io from 'socket.io-client';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-top-streams',
@@ -6,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-streams.component.css']
 })
 export class TopStreamsComponent implements OnInit {
+  topPosts: any;
+  socket: any;
+  user: any;
 
-  constructor() { }
+  constructor(private postService: PostService, private tokenService: TokenService, private router: Router) {
+    this.socket = io('http://localhost:3000');
+  }
 
   ngOnInit() {
+    this.user = this.tokenService.GetPayload();
+    this.AllPosts();
+
+    this.socket.on('refreshPage', (data) => {
+      this.AllPosts();
+    });
+  }
+
+  AllPosts() {
+    this.postService.getAllPosts().subscribe(data => {
+      this.topPosts = data.top;
+    }, err => {
+      if(err.error.token === null) {
+        this.tokenService.DeleteToken();
+        this.router.navigate(['']);
+      }
+    });
+  }
+
+  TimeFromNow(time) {
+    return moment(time).fromNow();
+  }
+
+  CheckInArray(arr, username) {
+    return _.some(arr, {username: username});
+  }
+
+  LikePost(post) {
+    this.postService.addLike(post).subscribe(data => {
+      this.socket.emit('refresh', {});
+    }, err => console.log(err));
+  }
+
+  OpenCommentBox(post) {
+    this.router.navigate(['post', post._id]);
   }
 
 }
