c925b6a4f8592442787e26916c0f1900634ba4ba
Zinzidane <ya-gans@mail.ru>
2018-10-11 12:39:34 +0300

    Add notifications to dropdown

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from '../../services/token.service';
 import * as M from 'materialize-css';
+import { UsersService } from 'src/app/services/users.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,8 +12,9 @@ import * as M from 'materialize-css';
 })
 export class ToolbarComponent implements OnInit {
   user: any;
+  notifications = [];
 
-  constructor(private router: Router, private tokenService: TokenService) { }
+  constructor(private router: Router, private tokenService: TokenService, private usersService: UsersService) { }
 
   ngOnInit() {
     this.user = this.tokenService.GetPayload();
@@ -22,11 +25,23 @@ export class ToolbarComponent implements OnInit {
       hover: true,
       coverTrigger: false
     });
+
+    this.GetUser();
+  }
+
+  GetUser() {
+    this.usersService.GetUserById(this.user._id).subscribe(data => {
+      this.notifications = data.result.notifications.reverse();
+    });
   }
 
   logout() {
     this.tokenService.DeleteToken();
     this.router.navigate(['/']);
+  }
+
+  TimeFromNow(time) {
+    return moment(time).fromNow();
   }
 
   GoToHome() {
