d99f2c01cfb045ec1ad6b1569ed0705a6477a472
Zinzidane <ya-gans@mail.ru>
2018-10-12 18:06:14 +0300

    Add user typing feature

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TokenService } from 'src/app/services/token.service';
 import { MessageService } from 'src/app/services/message.service';
@@ -11,12 +11,14 @@ import io from 'socket.io-client';
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
-export class MessageComponent implements OnInit {
+export class MessageComponent implements OnInit, AfterViewInit {
   messageForm: FormGroup;
   receiver: any;
   user: any;
   messages = [];
   socket: any;
+  typingMessage;
+  typing = false;
 
   constructor(
     private fb: FormBuilder,
@@ -37,6 +39,27 @@ export class MessageComponent implements OnInit {
     this.socket.on('refreshPage', () => {
       this.GetUserByUsername(this.receiver.username);
     });
+
+    this.socket.on('is_typing', data => {
+      if(data.sender === this.receiver.username) {
+        this.typing = true;
+      }
+    });
+
+    this.socket.on('has_stopped_typing', data => {
+      if(data.sender === this.receiver.username) {
+        this.typing = false;
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    const params = {
+      room1: this.user.username,
+      room2: this.receiver.username
+    };
+
+    this.socket.emit('join chat', params);
   }
 
   init() {
@@ -66,5 +89,23 @@ export class MessageComponent implements OnInit {
         this.messageForm.reset();
       });
     }
+  }
+
+  IsTyping() {
+    this.socket.on('start_typing', {
+      sender: this.user.username,
+      receiver: this.receiver.username
+    });
+
+    if(this.typingMessage) {
+      clearTimeout(this.typingMessage);
+    }
+
+    this.typingMessage = setTimeout(() => {
+      this.socket.emit('stop_typing', {
+        sender: this.user.username,
+        receiver: this.receiver.username
+      });
+    }, 500);
   }
 }
