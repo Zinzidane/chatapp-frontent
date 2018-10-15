c23f644a2a0a2b5f3da0c8ac8ab92611c3bd6bd4
Zinzidane <ya-gans@mail.ru>
2018-10-15 12:31:27 +0300

    Import emoji picker

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -20,6 +20,7 @@ import { ChatComponent } from '../components/chat/chat.component';
 import { MessageComponent } from '../components/message/message.component';
 import { MessageService } from '../services/message.service';
 import { NgxAutoScrollModule } from 'ngx-auto-scroll';
+import { EmojiPickerModule } from 'angular2-emoji-picker';
 
 @NgModule({
   imports: [
@@ -28,7 +29,8 @@ import { NgxAutoScrollModule } from 'ngx-auto-scroll';
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule,
-    NgxAutoScrollModule
+    NgxAutoScrollModule,
+    EmojiPickerModule.forRoot()
   ],
   declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent, PostsComponent, CommentsComponent, PeopleComponent, FollowersComponent, NotificationsComponent, TopStreamsComponent, ChatComponent, MessageComponent],
   exports: [StreamsComponent, ToolbarComponent],
