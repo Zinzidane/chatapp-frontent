f495d876afb69c3cab4a6f900b802f23f403539c
Zinzidane <ya-gans@mail.ru>
2018-10-23 20:30:58 +0300

    Add profile notifications methode

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -75,5 +75,10 @@ export class PeopleComponent implements OnInit {
 
   ViewUser(user) {
     this.router.navigate([user.username]);
+    if(this.loggedInUser.username !== user.username) {
+      this.userService.ProfileNotifications(user._id).subscribe(data => {
+        this.socket.emit('refresh', {});
+      }, err => console.log(err));
+    }
   }
 }
